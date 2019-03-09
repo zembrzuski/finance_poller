c528ae05cf6e3c0ebac0550d27f13a385eacc284
zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2019-03-09 13:07:20 -0300

    date wip

---
+++ src/config/config.py
@@ -0,0 +1,9 @@
+yahoo_url_for_polling = "https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&crumb=U4e8eDQi%2FyI&" \
+                        "lang=en-US&region=US&symbols=" \
+                        "{}" \
+                        "&fields=messageBoardId%2ClongName%2CshortName%2CmarketCap%2CunderlyingSymbol%2CunderlyingExchangeSymbol%2CheadSymbolAsString%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CregularMarketVolume%2Cuuid%2CregularMarketOpen%2CfiftyTwoWeekLow%2CfiftyTwoWeekHigh&corsDomain=finance.yahoo.com"
+
+companies = [
+    'PETR4.SA',
+    'GGBR4.SA'
+]
--- src/main.py
+++ src/main.py
@@ -1,5 +1,33 @@
+import requests
+import src.config.config as config
+import datetime
+import pytz
+
+
+def process_datetime(raw):
+    local = pytz.timezone("America/Sao_Paulo")
+    my_datetime_brt = datetime.datetime.fromtimestamp(raw)
+    local_dt = local.localize(my_datetime_brt, is_dst=None)
+    brt_datetime = local_dt.astimezone(pytz.utc)
+
+    formatted = brt_datetime.strftime("%Y-%m-%dT%H:%M:%S.000")
+    print(formatted)
+
+
+def process_a_company(company):
+    process_datetime(company['regularMarketTime']['raw'])
+    print('oi')
+    pass
+
+
 def main():
-    print('hello world')
+    companies_array = ','.join(config.companies)
+    resp = requests.get(config.yahoo_url_for_polling.format(companies_array)).json()
+
+    for company in resp['quoteResponse']['result']:
+        process_a_company(company)
+
+    print(resp)
 
 
 if __name__ == '__main__':
