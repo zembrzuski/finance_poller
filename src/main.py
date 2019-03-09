e6058c45bac53277731c26b60c1b4f030b28771f
zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2019-03-09 14:47:37 -0300

    refactorign

--- src/main.py
+++ src/main.py
@@ -1,27 +1,10 @@
-import requests
 import src.config.local as config
-import src.service.helper.company_converter as company_converter
-import src.repository.company_repository as company_repository
-
-
-def process_chunk(companies):
-    companies_array = ','.join(companies)
-    companies_retrieved = requests.get(config.yahoo_url_for_polling.format(companies_array)).json()
-
-    companies_to_persist = list(map(
-        lambda x: company_converter.convert_company_from_yahoo_to_elasticsearch_entity(x),
-        companies_retrieved['quoteResponse']['result']))
-
-    persistence_result = list(map(
-        lambda x: company_repository.persist_company_on_elasticsearch(x),
-        companies_to_persist))
-
-    print(persistence_result)
+import src.service.yahoo_poller as yahoo_poller
 
 
 def main():
     companies_chunks = [config.companies[x:x + 10] for x in range(0, len(config.companies), 10)]
-    list(map(lambda chunk: process_chunk(chunk), companies_chunks))
+    list(map(lambda chunk: yahoo_poller.process_chunk(chunk), companies_chunks))
 
 
 if __name__ == '__main__':
---
+++ src/service/yahoo_poller.py
@@ -0,0 +1,19 @@
+import requests
+import src.service.helper.company_converter as company_converter
+import src.repository.company_repository as company_repository
+import src.config.local as config
+
+
+def process_chunk(companies):
+    companies_array = ','.join(companies)
+    companies_retrieved = requests.get(config.yahoo_url_for_polling.format(companies_array)).json()
+
+    companies_to_persist = list(map(
+        lambda x: company_converter.convert_company_from_yahoo_to_elasticsearch_entity(x),
+        companies_retrieved['quoteResponse']['result']))
+
+    persistence_result = list(map(
+        lambda x: company_repository.persist_company_on_elasticsearch(x),
+        companies_to_persist))
+
+    print(persistence_result)
