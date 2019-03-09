e7c251c5fb249d20c2c2e768f4911790f4a586d6
zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2019-03-09 14:38:05 -0300

    processing in chunks

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ import src.config.local as config
 import src.service.helper.company_converter as company_converter
 import src.repository.company_repository as company_repository
 
-def main():
-    companies_array = ','.join(config.companies)
+
+def process_chunk(companies):
+    companies_array = ','.join(companies)
     companies_retrieved = requests.get(config.yahoo_url_for_polling.format(companies_array)).json()
 
     companies_to_persist = list(map(
@@ -15,7 +16,12 @@ def main():
         lambda x: company_repository.persist_company_on_elasticsearch(x),
         companies_to_persist))
 
-    print(companies_retrieved)
+    print(persistence_result)
+
+
+def main():
+    companies_chunks = [config.companies[x:x + 10] for x in range(0, len(config.companies), 10)]
+    list(map(lambda chunk: process_chunk(chunk), companies_chunks))
 
 
 if __name__ == '__main__':
